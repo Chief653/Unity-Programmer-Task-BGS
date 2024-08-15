d278f4dc98900281e2e90648a3e7edaebe70a0fd
Leonardo Pedroso <101359626+Chief653@users.noreply.github.com>
2024-08-15 16:37:27 -0300

    Initial commit

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public enum ItemType
+{
+    Tool,
+    Consumable,
+    Armor
+}
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    public int itemId;
+    public string itemName;
+    public ItemType itemType;
+    public Sprite itemIcon;
+}
